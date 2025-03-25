e8fa27e83236787c21c949bd3227e359e08b425b
MAAghakabiri <a.aghakabiri@gmail.com>
2025-03-25 14:04:10 +0330

    add

---
+++ Tools/AuthoraizationTools/TokenGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Tools.AuthoraizationTools
+{
+    public class TokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateAccessToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // خواندن مقدار JWTSecret از فایل appsettings.json
+            var secretKey = _configuration["JWTSettings:JWTSecret"];
+            var key = Encoding.UTF8.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, username)
+            }),
+                Expires = DateTime.UtcNow.AddMinutes(15), // مدت زمان Access Token
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
