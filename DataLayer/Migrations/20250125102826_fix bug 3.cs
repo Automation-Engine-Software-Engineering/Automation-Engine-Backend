2c5832d7abd61573a0a865c9e06e745c2845f73a
Soheil Salimi <soheilsalimidev@gmail.com>
2025-04-30 22:22:38 +0330

    add context

---
+++ DataLayer/Migrations/20250125102826_fix bug 3.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixbug3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenueElements");
+
+            migrationBuilder.CreateTable(
+                name: "MenuElements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MenuType = table.Column<int>(type: "int", nullable: false),
+                    ParentMenuElemntId = table.Column<int>(type: "int", nullable: true),
+                    WorkflowId = table.Column<int>(type: "int", nullable: true),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuElements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuElements_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MenuElements_Workflow_WorkflowId",
+                        column: x => x.WorkflowId,
+                        principalTable: "Workflow",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuElements_RoleId",
+                table: "MenuElements",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuElements_WorkflowId",
+                table: "MenuElements",
+                column: "WorkflowId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuElements");
+
+            migrationBuilder.CreateTable(
+                name: "MenueElements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    WorkflowId = table.Column<int>(type: "int", nullable: true),
+                    MenueType = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ParentMenueElemntId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenueElements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenueElements_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MenueElements_Workflow_WorkflowId",
+                        column: x => x.WorkflowId,
+                        principalTable: "Workflow",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenueElements_RoleId",
+                table: "MenueElements",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenueElements_WorkflowId",
+                table: "MenueElements",
+                column: "WorkflowId");
+        }
+    }
+}
