1c92b8bb6b3dadeec7ffdfd784e3ad04b7b6023e
Soheil Salimi <soheilsalimidev@gmail.com>
2025-05-03 23:04:10 +0330

    add migration

---
+++ DataLayer/Migrations/20250126155939_add link.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "link",
+                table: "MenuElements",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "link",
+                table: "MenuElements");
+        }
+    }
+}
